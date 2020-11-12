dd43e2e17eccc8bd06a1978bb42a226967e19fd6
qiaoyu.peng <qiaoyu.peng@hex-tech.net>
2020-11-12 09:59:36 +0800

    update config directory

--- pkg/settings.go
+++ pkg/settings.go
@@ -5,7 +5,7 @@ import "github.com/spf13/viper"
 func InitSettings() error {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath("config/")
+	viper.AddConfigPath("configs/")
 	err := viper.ReadInConfig()
 	return err
 }
